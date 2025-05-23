f9d656d244f47af7fef21e22d1b86eff9e113377
guilhermemarcal <guilherme.marcal.barbosa@escola.pr.gov.br>
2025-05-23 10:29:15 -0300

    23/05

---
+++ perguntas.js
@@ -0,0 +1,19 @@
+criaCartao(    
+    'O que são células eucariontes?',
+    'São aquelas células que tem núcleo'
+)
+
+criaCartao(
+'Qual o nome científico do hipopótamo?',    
+'Hippopotamus Amphibius'
+)
+
+criaCartao(
+    'Qual vírus causa a gripe?',
+    'É  o vírus Influenza que causa a gripe.'
+)
+
+criaCartao(
+    'Porque o filho biológico se parece com os pais biológicos?',
+    'Porque ele herdou a característica genética dos pais.'
+)
